366a54c4cdb4a7f2274d6ab297ab2f66440c9898
Maciej Kucmus <maciejkucmus@gmail.com>
2020-03-08 11:09:13 +0100

    feat: deploy comment on PR

--- index.js
+++ index.js
@@ -9,19 +9,27 @@ const client = axios.create({
 });
 const delay = ms => new Promise(r => setTimeout(r, ms));
 
+const upsertDeployComment = async (client, repo, commitHash) => {
+  const { data: comments } = await client.repos.listCommentsForCommit({
+    ...repo,
+    commit_sha: commitHash
+  });
+}
+
 ;(async function() {
   try {
     const githubToken = core.getInput('token');
     const namespace = core.getInput('namespace');
-    const prNumber = github.context.payload.pull_request && github.context.payload.pull_request.number
-    const dockerImageHash = github.context.sha;
+    const { sha: commitHash, repo, payload } = github.context
+
+    const prNumber = payload.pull_request && payload.pull_request.number
 
     if (!githubToken || !prNumber || !namespace) {
       core.setFailed('Some action arguments are missing. Action has failed.');
       return;
     }
 
-    const deployUrl = `https://${dockerImageHash}.${namespace}.storefrontcloud.io`
+    const deployUrl = `https://${commitHash}.${namespace}.storefrontcloud.io`
     console.log(`Starting deploying PR #${prNumber} on ${deployUrl}`);
     
     await client.get(deployUrl); // double request - temporary cloud's fix
@@ -32,8 +40,10 @@ const delay = ms => new Promise(r => setTimeout(r, ms));
     }    
     
     console.log(`Your application is successfully deployed.`);
-    // const octokit = new github.GitHub(githubToken);
+    const octokit = new github.GitHub(githubToken);
     // TODO: handle PR comment update.
+    await upsertDeployComment(octokit, repo, commitHash);
+
     core.setOutput('preview_url', deployUrl);
    
   } catch (error) {
