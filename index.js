93a4fdb32abd14c77a0c54084a82e5a4d89ab5c1
Maciej Kucmus <maciejkucmus@gmail.com>
2020-03-08 14:59:29 +0100

    feat: upsert deploy comment

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ const github = require('@actions/github');
 const axios = require('axios');
 const https = require('https');
 const client = axios.create({
-  httpsAgent: new https.Agent({  
-    rejectUnauthorized: false
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false // double request - temporary cloud's fix
   })
 });
+const DEPLOY_COMMENT_TEMPLATE = ':blue_heart: NAMESPACE successfully deployed';
 const delay = ms => new Promise(r => setTimeout(r, ms));
-
-const upsertDeployComment = async (client, repo, commitHash) => {
+const getDeployUrl = (version, namespace) => `https://${version}.${namespace}.storefrontcloud.io`
+const upsertDeployComment = async (client, repo, commitHash, deployUrl, namespace) => {
   const { data: comments } = await client.repos.listCommentsForCommit({
     ...repo,
     commit_sha: commitHash
   });
+
+  const { id } = comments.find(({body}) => body.startsWith(DEPLOY_COMMENT_TEMPLATE))
+  const newCommentBody = `${DEPLOY_COMMENT_TEMPLATE.replace('NAMESPACE', namespace)} at ${deployUrl}`
+  if (!id) {
+    await client.repos.createCommitComment({
+      repo,
+      commit_sha: commitHash,
+      body: newCommentBody
+    });
+  } else {
+    await client.repos.updateCommitComment({
+      repo,
+      comment_id: id,
+      body: newCommentBody
+    });
+  }
 }
 
 ;(async function() {
@@ -29,12 +46,12 @@ const upsertDeployComment = async (client, repo, commitHash) => {
       return;
     }
 
-    const deployUrl = `https://${commitHash}.${namespace}.storefrontcloud.io`
+    const deployUrl = getDeployUrl(commitHash, namespace)
     console.log(`Starting deploying PR #${prNumber} on ${deployUrl}`);
     
     await client.get(deployUrl); // double request - temporary cloud's fix
-    await delay(3000) // double request - temporary cloud's fix
-    const response = await client.get(deployUrl);
+    await delay(3000) 
+    const response = await client.get(deployUrl); // double request - temporary cloud's fix
     if (!response.data.includes('<html data-n-head-ssr')) {
       throw "Deploy has failed. Application returns wrong data."
     }    
@@ -42,10 +59,8 @@ const upsertDeployComment = async (client, repo, commitHash) => {
     console.log(`Your application is successfully deployed.`);
     const octokit = new github.GitHub(githubToken);
     // TODO: handle PR comment update.
-    await upsertDeployComment(octokit, repo, commitHash);
-
+    await upsertDeployComment(octokit, repo, commitHash, deployUrl, namespace);
     core.setOutput('preview_url', deployUrl);
-   
   } catch (error) {
     core.setFailed(error.message);
   }
