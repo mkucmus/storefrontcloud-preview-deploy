2ea1d294afebacb17c0a4088a09cb86a91d513cc
Maciej Kucmus <maciejkucmus@gmail.com>
2020-03-16 08:32:11 +0100

    refactor: remove unused functions

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
-const https = require('https');
-// const client = axios.create({
-//   httpsAgent: new https.Agent({
-//     rejectUnauthorized: false // double request - temporary cloud's fix
-//   })
-// });
+
 const delay = ms => new Promise(r => setTimeout(r, ms));
 const getDeployUrl = (version, namespace) => `https://${version}.${namespace}.preview.storefrontcloud.io`
-const isPush = ({eventName, issue: { number }}) => {
-  if (number && eventName !== 'push') {
-    return false;
-  }
-  
-  return true;
-}
-const upsertDeployComment = async (client, repo, commitHash, deployUrl, namespace, isPush, issue) => {
-  const { data: comments } = await client.repos.listCommentsForCommit({
-    ...repo,
-    commit_sha: commitHash
-  });
-  //core.debug(comments);
-  const DEPLOY_COMMENT_TEMPLATE = ':blue_heart: shopware-pwa successfully deployed';
-  const oldComment = comments.find(({body}) => body.startsWith(DEPLOY_COMMENT_TEMPLATE))
-  const newCommentBody = `${DEPLOY_COMMENT_TEMPLATE} at ${deployUrl}`
-  if (!oldComment) {
-    core.info(`deployment comment does not exist. creating new one.`)
-    isPush && await client.repos.createCommitComment({
-      ...repo,
-      commit_sha: commitHash,
-      body: newCommentBody
-    }) || await client.issues.createComment({ // or PR
-      ...repo,
-      issue_number: issue.number,
-      body: newCommentBody
-    });
 
-  } else { // update existing
-    core.info(`deployment comment already exists. updating it with new deploy URL.`)
-    isPush && await client.repos.updateCommitComment({
-      ...repo,
-      comment_id: oldComment.id,
-      body: newCommentBody
-    }) || await client.issues.updateComment({ // or PR
-      ...repo,
-      comment_id: oldComment.id,
-      body: newCommentBody
-    })
-
-  }
-}
 
 ;(async function() {
 
